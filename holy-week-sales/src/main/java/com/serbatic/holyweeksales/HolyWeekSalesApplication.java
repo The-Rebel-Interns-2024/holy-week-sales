730aed3f696d7a78b4a6065d3c49d8a6fbc3ea77
Sofia-Arcones <sarcones@serbatic.es>
2024-03-25 10:07:11 +0100

    Base Project

---
+++ holy-week-sales/src/main/java/com/serbatic/holyweeksales/HolyWeekSalesApplication.java
@@ -0,0 +1,13 @@
+package com.serbatic.holyweeksales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyWeekSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyWeekSalesApplication.class, args);
+	}
+
+}
---
+++ holy-week-sales/src/test/java/com/serbatic/holyweeksales/HolyWeekSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serbatic.holyweeksales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolyWeekSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
