380d07dc83ea2a1fc25d688671cc04d2ba05f6e1
Sofia-Arcones <sarcones@serbatic.es>
2024-03-27 12:23:53 +0100

    build:client-feing-product

---
+++ holy-week-sales/src/main/java/com/serbatic/holyweeksales/client/StorehouseFeingClient.java
@@ -0,0 +1,16 @@
+package com.serbatic.holyweeksales.client;
+
+import com.serbatic.holyweeksales.presentation.dto.ProductStorageResource;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "storehouse-feing-client" , url = "${storehouse.service.url}")
+public interface StorehouseFeingClient {
+
+    @PostMapping("/product")
+    ResponseEntity<ProductStorageResource> createProduct(@RequestBody ProductStorageResource productStorageResource);
+
+
+}
