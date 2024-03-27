abaa758d0923eece34a85d83e868d60045c9159c
Sofia-Arcones <sarcones@serbatic.es>
2024-03-27 12:26:56 +0100

    Revert "build:client-feing-product"
    This reverts commit 380d07dc83ea2a1fc25d688671cc04d2ba05f6e1.

--- holy-week-sales/src/main/java/com/serbatic/holyweeksales/HolyWeekSalesApplication.java
+++ holy-week-sales/src/main/java/com/serbatic/holyweeksales/HolyWeekSalesApplication.java
@@ -2,10 +2,8 @@ package com.serbatic.holyweeksales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class HolyWeekSalesApplication {
 
 	public static void main(String[] args) {
