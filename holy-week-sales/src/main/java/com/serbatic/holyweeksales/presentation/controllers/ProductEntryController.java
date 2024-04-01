9f4b833e835fe7e86abcc801c906c467f3cb7d71
lgarciaSB <163408898+lgarciaSB@users.noreply.github.com>
2024-04-01 12:11:09 +0200

    build: POST create entry product

---
+++ holy-week-sales/src/main/java/com/serbatic/holyweeksales/presentation/controllers/ProductEntryController.java
@@ -0,0 +1,30 @@
+package com.serbatic.holyweeksales.presentation.controllers;
+
+import com.serbatic.holyweeksales.business.services.product.ProductService;
+import com.serbatic.holyweeksales.business.services.productEntry.ProductEntryService;
+import com.serbatic.holyweeksales.data.entities.Product;
+import com.serbatic.holyweeksales.presentation.dto.ProductEntryResponse;
+import com.serbatic.holyweeksales.presentation.dto.StorageResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/entries")
+public class ProductEntryController {
+    @Autowired
+    ProductEntryService productEntryService;
+
+    @PostMapping()
+    public ResponseEntity<ProductEntryResponse> createEntryProduct(@RequestBody StorageResource productR) {
+        if(productR.getQuantity() > 0){
+            return ResponseEntity.ok(productEntryService.save(productR));
+        } else{
+            throw new IllegalArgumentException("The quantity is not valid.");
+        }
+
+    }
+}
